88a1a4d90fc97d2cf919ea2895d78d3e153aad1a
wan_z <1297285962qq.com>
2022-05-06 17:50:48 +0800

    代码格式化配置

--- wk.config.js
+++ wk.config.js
@@ -1,11 +1,11 @@
 const path = require('path')
 
 module.exports = {
-    // 指定入口文件
-    entry: "./src/index.js",
-    // 指定出口文件
-    output: {
-        path: path.resolve(__dirname, './dist'),  // 绝对路径
-        filename: 'boundle.js'
-    }
+  // 指定入口文件
+  entry: "./src/index.js",
+  // 指定出口文件
+  output: {
+    path: path.resolve(__dirname, './build'),  // 绝对路径
+    filename: 'boundle.js'
+  }
 }
